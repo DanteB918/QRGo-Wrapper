8654959e17207fad866b45994cfbcd9c89943aa8
Dante Bradshaw <plansuperior@gmail.com>
2024-02-07 05:02:16 -0500

    URLs are encoded, return type param for qr read method

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,11 @@
 ## [0.1.0] - 2023-11-06
 
 - Initial release
+
+## [0.1.1] - 2024-02-06
+
+- Minor bug fixes
+
+## [0.1.2] - 2024-02-07
+
+- URLs are encoded, return type param for qr read method
--- lib/qrcode.rb
+++ lib/qrcode.rb
@@ -6,31 +6,36 @@ require 'json'
 module QRCode
   @base_url = "https://api.qrserver.com/v1/"
 
+  # @param [String] url = URL of the image of the QR Code
+  # @param [Integer] width = width of qr code image
+  # @param [Integer] url = height of qr code image
   def self.qr_create(url, width = 100, height = 100)
-    uri = URI("#{@base_url}create-qr-code/?data=#{url}&size=#{width.to_s}x#{height.to_s}")
+    uri = URI("#{@base_url}create-qr-code/?data=#{CGI.escape(url)}&size=#{width.to_s}x#{height.to_s}")
 
     begin
-        res = Net::HTTP.get_response(uri)
+      res = Net::HTTP.get_response(uri)
     rescue
-        return "Failed to connect with API endpoint."
+      return "Failed to connect with API endpoint."
     end
 
     return res.body if res.is_a?(Net::HTTPSuccess)
   end
 
-  def self.qr_read(url)
-    uri = URI("#{@base_url}read-qr-code/?fileurl=#{url}")
+  # @param [String] url = URL of the image of the QR Code
+  # @param [String] output = "json" or "xml"
+  def self.qr_read(url, output = 'json')
+    uri = URI("#{@base_url}read-qr-code/?fileurl=#{CGI.escape(url)}&outputformat=#{output}")
 
     begin
-        res = Net::HTTP.get_response(uri)
+      res = Net::HTTP.get_response(uri)
     rescue
-        return "Failed to connect with API endpoint."
+      return "Failed to connect with API endpoint."
     end
 
     parsed_json = JSON.parse(res.body)[0]["symbol"][0];
 
     if ! parsed_json['error'].nil?
-        return "Error: " + parsed_json['error'].to_s
+      return "Error: " + parsed_json['error'].to_s
     end
 
     return parsed_json['data'].to_s if res.is_a?(Net::HTTPSuccess)
